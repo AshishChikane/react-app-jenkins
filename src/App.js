00edd0a8d0ab6d7e741fe99f7dd4422cd3d6036a
Ashish <ashish.c@tagloy.com>
2024-09-25 11:24:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React & Jenkins With ashish 
         </a>
       </header>
     </div>
