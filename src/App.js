f471a02de5c4a603fb0ce994ce00894ce3388dec
Ashish <ashish.c@tagloy.com>
2024-09-26 12:05:01 +0530

    trigger check

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Ashish 26 sept 2024 updated build with webhook
+          Learn React with Ashish 26 sept 2024 updated build with webhook trigger check
         </a>
       </header>
     </div>
