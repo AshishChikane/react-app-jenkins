ef9142f6a9a17f5293688728a7557f3b06a07d1b
Ashish <ashish.c@tagloy.com>
2024-09-26 12:00:53 +0530

    date update webhook

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Ashish 26 sept 2024 updated
+          Learn React with Ashish 26 sept 2024 updated build with webhook
         </a>
       </header>
     </div>
